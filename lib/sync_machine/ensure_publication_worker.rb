adb880aaec635e10003548bd763b4568326fdafe
Francis Hwang <sera@fhwang.net>
2019-03-23 10:01:29 -0400

    Extract Deduper and Publisher.

--- lib/sync_machine/ensure_publication_worker.rb
+++ lib/sync_machine/ensure_publication_worker.rb
@@ -11,9 +11,6 @@ module SyncMachine
       define_singleton_method(hook) do |&block|
         self.hooks[hook] = block
       end
-      define_method(hook) do |*args|
-        self.class.hooks[hook].call(*args)
-      end
     end
 
     def self.inherited(subclass)
@@ -23,17 +20,27 @@ module SyncMachine
     def perform(subject_id, enqueue_time_str)
       @subject_id = subject_id
       @enqueue_time_str = enqueue_time_str
-      dedupe do
-        if check_publishable(subject)
-          payload_body = build(subject)
+      deduper = Deduper.new(
+        job_class: self.class,
+        previous_payload: previous_payload,
+        subject_id: subject_id,
+        enqueue_time_str: enqueue_time_str
+      )
+      deduper.dedupe do
+        if self.class.hooks[:check_publishable].call(subject)
+          payload_body = self.class.hooks[:build].call(subject)
           if same_payload_body?(payload_body)
             previous_payload.update_attribute(:generated_at, Time.now)
           else
-            publish(subject, payload_body)
-            save_payload_body(payload_body)
-            if hook_defined?(:after_publish)
-              after_publish(subject)
-            end
+            publisher = Publisher.new(
+              after_publish_hook: self.class.hooks[:after_publish],
+              payload_body: payload_body,
+              payload_class: payload_class,
+              previous_payload: previous_payload,
+              publish_hook: self.class.hooks[:publish],
+              subject: subject
+            )
+            publisher.publish
           end
         end
       end
@@ -41,34 +48,8 @@ module SyncMachine
 
     private
 
-    def dedupe(&block)
-      redis_lock = "#{self.class.name}:#{@subject_id}"
-      lock_acquired = Redis.current.set(
-        redis_lock, "true", nx: true, ex: 10.minutes
-      )
-      if lock_acquired
-        begin
-          if !performed_since_enqueue_time?
-            block.call
-          end
-        ensure
-          Redis.current.del(redis_lock)
-        end
-      else
-        self.class.perform_in(1 + rand(10), @subject_id, @enqueue_time_str)
-      end
-    end
-
     def enqueue_time
       @enqueue_time ||= Time.parse(@enqueue_time_str)
-    end
-
-    def hook_defined?(hook)
-      !!self.class.hooks[hook]
-    end
-
-    def performed_since_enqueue_time?
-      previous_payload && previous_payload.generated_at > enqueue_time
     end
 
     def payload_class
@@ -86,18 +67,6 @@ module SyncMachine
       if previous_payload
         HashWithIndifferentAccess.new(previous_payload.body) ==
           HashWithIndifferentAccess.new(payload_body)
-      end
-    end
-
-    def save_payload_body(payload_body)
-      if previous_payload
-        previous_payload.update_attributes(
-          body: payload_body, generated_at: Time.now
-        )
-      else
-        payload_class.create!(
-          body: payload_body, generated_at: Time.now, subject_id: @subject_id
-        )
       end
     end
 
