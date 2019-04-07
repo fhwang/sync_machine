64c507149ee00c33466b9c0c595ddcd7678af649
Francis Hwang <sera@fhwang.net>
2019-04-07 17:13:58 -0400

    WIP

--- lib/sync_machine/orm_adapter/mongoid.rb
+++ lib/sync_machine/orm_adapter/mongoid.rb
@@ -8,13 +8,7 @@ module SyncMachine
       end
 
       def self.change_listener_changed_keys(record)
-        changed_keys = record.changes.keys
-        record.class.reflect_on_all_associations.each do |assoc|
-          if assoc.macro == :embeds_one && record.send(assoc.name).try(:changed?)
-            changed_keys << assoc.name
-          end
-        end
-        changed_keys
+        record.changes.keys
       end
 
       def self.record_id_for_job(record_id)
